2e950fd5f41d43d69ba614a77dd92e71807f1c84
VANOVERBEKE <joost.vanoverbeke@inbo.be>
2020-05-06 12:56:44 +0200

    herstellen project

---
+++ moneos_2020_test/src/markdown/pad.R
@@ -0,0 +1,16 @@
+
+
+# pad pc joost
+pad_prj_schelde <- "G:/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
+# pad citrix joost
+# basis_pad <- "//Client/G$/Mijn Drive/INBODATA/PROJECTEN/PRJ_SCHELDE/"
+
+pad_moneos <- "VNSC/Rapportage_INBO/2020/"
+
+maak_pad <- function(hoofdstuk, onderdeel = NULL) {
+  pad <- paste0(pad_prj_schelde, pad_moneos, hoofdstuk, "/")
+  if (!is.null(onderdeel))
+    pad <- paste0(pad, onderdeel, "/")
+  return(pad)
+}
+
